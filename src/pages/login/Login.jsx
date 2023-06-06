80c8da1116470fadc031fd3fb9eee6006e2ab2f2
Ridoy Sharif <developersridoy@gmail.com>
2023-06-07 04:45:03 +0600

    bg image change

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 //import loginImg from '../../assets/login.png'
-const bgImage =`https://img.freepik.com/free-vector/isometric-e-commerce-concept_52683-39811.jpg?w=900&t=st=1685985549~exp=1685986149~hmac=315e1b3ce65a9c911441cabc56adaec6cbe1ad77c5370eed769690b7beeaa91b`
+const bgImage =`https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg?w=900&t=st=1686065798~exp=1686066398~hmac=49de08e1ec1392f4b6527388784cb99430c4a629bb8e5b18a72c65a06e3e6923`
 
 const Login = () => {
 
@@ -28,7 +28,7 @@ const Login = () => {
                                   placeholder="Enter your email" 
                                   className="bg-transparent px-5 py-3 outline-none placeholder-white border border-white   rounded-full w-full" 
                               />
-                              {errors.email?.type === 'required' && <p className='text-red-600 pl-3 mt-1' role="alert">Email is required</p>}
+                              {errors.email?.type === 'required' && <p className='text-red-600 pl-3 mt-1' role="alert">Email field is required</p>}
                           </div>
                           <div>
                             <input 
@@ -39,7 +39,7 @@ const Login = () => {
                                   placeholder="Enter your password"
                                   className="bg-transparent px-5 py-3 outline-none placeholder-white border border-white rounded-full w-full" 
                               />
-                              {errors.password?.type === 'required' && <p className='text-red-600 pl-3 mt-1' role="alert">password is required</p>}
+                              {errors.password?.type === 'required' && <p className='text-red-600 pl-3 mt-1' role="alert">Password field is required</p>}
                           </div>
                           <input 
                                 className='py-2 block w-full px-10 rounded-full mt-5 cursor-pointer text-base font-medium glass' 
