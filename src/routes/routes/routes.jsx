1c6ffa62ab251ac0791b1815d869743782261113
Ridoy Sharif <developersridoy@gmail.com>
2023-06-06 20:33:23 +0600

    install successful and create some folder

---
+++ src/routes/routes/routes.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import Main from '../../layout/Main'
+import Home from '../../pages/home/home/Home'
+import Login from '../../pages/login/Login'
+import Registration from '../../pages/registration/Registration'
+
+const router=createBrowserRouter([
+    {
+        path:"/",
+        element:<Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'login',
+                element:<Login></Login>
+            },
+            {
+                path:'/registration',
+                element:<Registration></Registration>
+            },
+        ]
+    }
+])
+
+export default router
