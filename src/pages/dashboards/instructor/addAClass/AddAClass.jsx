d2d3f42badc9789d1cbe1c23522778241bd15098
Ridoy Sharif <developersridoy@gmail.com>
2023-06-08 02:08:06 +0600

    add a class form added complete

--- src/pages/dashboards/instructor/addAClass/AddAClass.jsx
+++ src/pages/dashboards/instructor/addAClass/AddAClass.jsx
@@ -1,35 +1,42 @@
 import React, { useContext } from 'react'
 import { AuthContext } from '../../../../context/AuthProvider'
+import { useForm } from 'react-hook-form';
 
 const AddAClass = () => {
 
     const {user} = useContext(AuthContext)
-console.log(user)
+    const {register,handleSubmit,formState: { errors },} = useForm();
+    const onSubmit = data =>{
+        console.log(data)
+    };
   return (
     <div>
         <div className='container mx-auto px-5'>
-            <div>
-                <form>
+            <div className='w-full lg:w-1/2 mx-auto'>
+                <form onSubmit={handleSubmit}>
                     <input 
                         type="text" 
                         name='class_name' 
-                        id='class_name' 
+                        id='class_name'
+                        {...register("class_name", { required: true})} 
                         placeholder="Class name" 
-                        className="input input-bordered w-full" 
+                        className="input mb-3 input-bordered w-full" 
                     />
                     <input 
                         type="text" 
                         name='class_image' 
                         id='class_image' 
+                        {...register("class_image", { required: true})} 
                         placeholder="Class Image" 
-                        className="input input-bordered w-full" 
+                        className="input mb-3 input-bordered w-full" 
                     />
                     <input 
                         type="text" 
                         name='instructor_name' 
                         id='instructor_name' 
+                        {...register("instructor_name", { required: true})} 
                         placeholder="Instructor Name" 
-                        className="input input-bordered w-full" 
+                        className="input mb-3 input-bordered w-full" 
                     />
                     <input 
                         type="text" 
@@ -37,23 +44,26 @@ console.log(user)
                         id='instructor_email'
                         defaultValue={user.email} 
                         placeholder="Instructor Email" 
-                        className="input disabled input-bordered w-full" 
+                        className="input mb-3 disabled bg-slate-300 input-bordered w-full" 
                         readOnly
                     />
                     <input 
-                        type="tel" 
+                        type="number" 
                         name='available_seats' 
                         id='available_seats'
+                        {...register("available_seats", { required: true})} 
                         placeholder="Available seats" 
-                        className="input input-bordered w-full" 
+                        className="input mb-3 input-bordered w-full" 
                     />
                     <input 
-                        type="text" 
+                        type="number" 
                         name='price' 
                         id='price'
+                        {...register("price", { required: true})} 
                         placeholder="Price" 
-                        className="input input-bordered w-full" 
+                        className="input mb-3 input-bordered w-full" 
                     />
+                    <button className='px-7 py-2 bg-black text-white rounded-md font-semibold text-base mt-5'>Add a class</button>
                 </form>
             </div>
         </div>
