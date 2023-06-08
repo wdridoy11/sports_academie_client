bf3ef1388802ed8332b042da6a3d85ec1e1a81fb
Ridoy Sharif <developersridoy@gmail.com>
2023-06-09 01:47:03 +0600

    gallery page created

---
+++ src/pages/home/gallery/Gallery.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Gallery = () => {
+  return (
+    <div>
+        <div className='container mx-auto px-5'>
+            <div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Gallery
--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './hero/Hero'
+import Gallery from '../gallery/Gallery'
 
 const Home = () => {
   return (
     <div>
       <Hero></Hero>
+      <Gallery></Gallery>
     </div>
   )
 }
