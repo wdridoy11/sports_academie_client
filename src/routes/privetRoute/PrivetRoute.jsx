095fc1a8bfa2429fa31912b1c2d2feca0e696c20
Ridoy Sharif <developersridoy@gmail.com>
2023-06-09 15:53:28 +0600

    privetRoute added

---
+++ src/routes/privetRoute/PrivetRoute.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import { AuthContext } from '../../context/AuthProvider'
+import { Navigate } from 'react-router-dom';
+
+const PrivetRoute = ({children}) => {
+
+    const {user, loading} = useState(AuthContext);
+
+    if(loading){
+        return <button className="btn btn-square">
+        <span className="loading loading-spinner"></span>
+      </button>
+    }
+
+    if(user){
+        return children
+    }
+
+  return <Navigate to={"/login"} replace={true}></Navigate>
+}
+
+export default PrivetRoute
