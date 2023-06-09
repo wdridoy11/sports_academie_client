c5165c8c67508fd526f6818a76d7cd6b060f05f6
Ridoy Sharif <developersridoy@gmail.com>
2023-06-10 03:54:38 +0600

    class data get and display

--- src/pages/classes/Classes.jsx
+++ src/pages/classes/Classes.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Cover from '../../components/shared/cover/Cover'
 import { FaUserCircle } from 'react-icons/fa';
 import { useContext } from 'react';
@@ -8,6 +8,18 @@ const coverImage = `https://img.freepik.com/free-photo/empty-classroom-due-coron
 
 const Classes = () => {
   const {user} = useContext(AuthContext);
+  const [classes, setClasses] = useState([]);
+
+  // classes data get from database
+  useEffect(()=>{
+    fetch(`http://localhost:5000/classes`)
+    .then((res)=>res.json())
+    .then((data)=>{
+      setClasses(data)
+    })
+  },[])
+
+
   const handleLoginCheck=()=>{
     if(!user){
       Swal.fire(
@@ -16,6 +28,12 @@ const Classes = () => {
         'error'
       )
     }else{
+      fetch(`http://localhost:5000/selects`,{
+        method:"POST",
+        headers:{
+          "content-type":"application/json"
+        }
+      })
       Swal.fire(
         'Congratulation',
         'Course selecting successful',
@@ -23,32 +41,35 @@ const Classes = () => {
       )
     }
   }
-  
+  console.log(classes)
   return (
     <div>
       <Cover coverImg={coverImage} title="Classes"></Cover>
         <div className='container mx-auto px-5 py-20'>
-            <div className='grid grid-cols-3 gap-10'>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-10'>
+              {classes.map((classInfo)=><>
                 <div className='rounded-lg'>
-                  <img className='w-full b-[300px] object-cover rounded-t-md' src={coverImage} alt="Sports_image" />
+                  <img className='w-full h-[300px] object-cover rounded-t-md' src={classInfo.class_image} alt="Sports_image" />
                   <div className='p-4 border rounded-b-lg'>
-                      <h3 className='text-2xl font-semibold mb-4'>Food Ball</h3>
+                      <h3 className='text-2xl font-semibold mb-4'>{classInfo.class_name}</h3>
                       <div className='flex justify-between'>
                           <div className='flex gap-2 items-center'>
                               <img className='w-7 h-7 object-cover rounded-full' src={coverImage} alt="Instructors" />
-                              <p className='text-base font-normal text-[#062015]'>Instructor name</p>
+                              <p className='text-base font-normal text-[#062015]'>{classInfo.instructor_name}</p>
                           </div>
                           <div className='flex gap-1 items-center'>
                               <FaUserCircle className='text-'></FaUserCircle>
-                              <p className='text-base font-normal text-[#062015]'>100</p>
+                              <p className='text-base font-normal text-[#062015]'>{classInfo.available_seats}</p>
                           </div>
                           <div>
-                              <p className='text-xl font-bold text-black'>$50</p>
+                              <p className='text-xl font-bold text-black'>${classInfo.price}</p>
                           </div>
                       </div>
                       <button onClick={handleLoginCheck} className='px-7 py-2 bg-black text-white rounded-md font-semibold text-base mt-5'>Select</button>
                   </div>
-                </div>
+                </div>             
+              </>)}
+
             </div>
         </div>
     </div>
