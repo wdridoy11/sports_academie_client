4959d88343ea7ad7f793f344ca939842f607107d
Ridoy Sharif <developersridoy@gmail.com>
2023-06-14 17:27:59 +0600

    all complete

--- src/hook/useAxiosSecure.jsx
+++ src/hook/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthProvider';
 
 const axiosSecure = axios.create({
-  baseURL: 'http://localhost:5000', 
+  baseURL: 'https://sports-academie-server.vercel.app/', 
 });
 
 const useAxiosSecure = () => {
